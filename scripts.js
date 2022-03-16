6a05e33da5f7440ba1564aae278b421e2dc61487
Xavier King <xking@canisbite.com>
2022-03-15 17:46:59 -0700

    changes  to script

--- scripts.js
+++ scripts.js
@@ -5,8 +5,36 @@
       container: 'map',
       // Replace YOUR_STYLE_URL with your style URL.
       style: 'mapbox://styles/xkingart/cl0rdp2ro000314oergmmt3cs', 
-      center: [-87.661557, 41.893748],
-      zoom: 10.7
+      center: [-99.18191837224161,19.425530543790487],
+      zoom: 10
     });
 
-    console.log("test")
+  
+
+
+function getData(){
+
+  return fetch('mexicoThings.json')
+  .then(response =>{
+    return response.json()
+  })
+};
+
+function getCord(){
+  getData()
+  .then (someCord => { //function(someCord) same thing
+      someCord.features.array.forEach(element => {
+        addMarker(element.geometry.coordinates);
+          
+      });
+  })
+};
+
+
+function addMarker(cords) {
+  var marker =  new mapboxgl.Marker()
+      .setLngLat(cords)
+      .addTo(map);
+};
+
+getCord();
